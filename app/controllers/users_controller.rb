a172a0767581b588dc976695169bb12a30c372bd
Wataru Miyamoto <ponmiyapon43@gmail.com>
2019-11-07 10:31:06 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
