a36a89e9eace3df9bb1277b32c2dfb225edc406d
Wataru Miyamoto <ponmiyapon43@gmail.com>
2019-11-11 14:37:33 +0000

    Finish add-basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   delete  '/logout', to: 'sessions#destroy'
   
   
-  resources :users
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
