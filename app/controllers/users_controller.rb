cf20668d1665e9a8a82370d7da02fc0753c6f67a
Wataru Miyamoto <ponmiyapon43@gmail.com>
2019-11-12 07:55:54 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
   
   def index
     @users = User.paginate(page: params[:page])
   end
   
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20191112003756) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-11-10 23:00:00"
-    t.datetime "work_time", default: "2019-11-10 22:30:00"
+    t.datetime "basic_time", default: "2019-11-11 23:00:00"
+    t.datetime "work_time", default: "2019-11-11 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
