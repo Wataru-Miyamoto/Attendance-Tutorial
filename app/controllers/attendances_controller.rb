929b3c56b4d6c0a7a27adce2134cafa2729dc6da
Wataru Miyamoto <ponmiyapon43@gmail.com>
2019-11-13 07:53:19 +0000

    Finish edit-attendanes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,8 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: :edit_one_month
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :set_one_month, only: :edit_one_month
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
@@ -20,4 +24,38 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
+  
+  def edit_one_month
+  end
+  
+  def update_one_month
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+  
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
+    
+    # beforeフィルター
+
+    # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end
+    end
 end
